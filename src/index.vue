eb7320220af163b6c2b8e3f7851b1c6ac6c4485d
Arnav Gupta <arnav@codingblocks.com>
2018-04-25 16:06:21 +0530

    autogenerated app
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ src/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="wrapper">
+    <image :src="logo" class="logo" />
+    <text class="greeting">The environment is ready!</text>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      logo: 'https://gw.alicdn.com/tfs/TB1yopEdgoQMeJjy1XaXXcSsFXa-640-302.png'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .wrapper {
+    justify-content: start;
+    align-items: center;
+  }
+  .logo {
+    width: 200px;
+    height: 100px;
+  }
+  .greeting {
+    text-align: center;
+    font-size: 20px;
+    color: #41B883;
+  }
+  .message {
+    margin: 30px;
+    font-size: 32px;
+    color: #727272;
+  }
+</style>
